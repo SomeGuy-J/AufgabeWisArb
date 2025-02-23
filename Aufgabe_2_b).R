3533081b04cc50dbcec604d40f9bafe6cf9f1d64
SomeGuy-J <jonathan.heinritz@gmx.de>
2025-02-23 12:55:49 +0100

    Korrektur
    "}" Zeichen die gefehlt haben bei beiden Funktionen eingefügt

--- Aufgabe_2_b).R
+++ Aufgabe_2_b).R
@@ -4,13 +4,13 @@ kategoriale_variablen <- function(data, var) {
   ##Überprüfen, ob die Variable ein Factor oder Character ist
   if (!is.factor(data[[var]]) && !is.character(data[[var]])) {
     stop("Die Variable muss ein Factor oder Character sein.")
-  }
-
-
-## Funktion 2) Prüft ob Variablen existieren
-
-bivariate_kategorial <- function(data, var1, var2) {
-  # Überprüfen, ob Variablen existieren
-  if (!(var1 %in% names(data)) || !(var2 %in% names(data))) {
-    stop("Eine oder beide Variablen sind nicht im Datensatz vorhanden.")
-  }
+  }}
+  
+  
+  ## Funktion 2) Prüft ob Variablen existieren
+  
+  bivariate_kategorial <- function(data, var1, var2) {
+    # Überprüfen, ob Variablen existieren
+    if (!(var1 %in% names(data)) || !(var2 %in% names(data))) {
+      stop("Eine oder beide Variablen sind nicht im Datensatz vorhanden.")
+    }}
